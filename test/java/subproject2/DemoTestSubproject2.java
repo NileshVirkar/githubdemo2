b2a41676fcaeef2af4edaf2f02bc5ef4e6bd1b8b
Nilesh <nilesh.virkar@acellere.com>
2023-11-06 16:09:08 +0530

    text

---
+++ test/java/subproject2/DemoTestSubproject2.java
@@ -0,0 +1,14 @@
+package subproject2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoTestSubproject2 {
+
+	@Test
+	public void testObject() {
+		assertTrue(false);
+	}
+
+}
